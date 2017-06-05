0dc7360160de650dbf0449f1c59e3056c818b2ad
Pavel Hübner <pavel.hubner@bigclown.com>
2017-06-05 19:06:23 +0200

    Fix Travis CI build

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ rm -rf out_release
 mkdir out_release
 
 make release PIXEL_COUNT=72
-mv out/firmware.bin out_release/firmware-72pixel.bin
+mv out/release/firmware.bin out_release/firmware-72pixel.bin
 
 make release PIXEL_COUNT=142
-mv out/firmware.bin out_release/firmware-142pixel.bin
+mv out/release/firmware.bin out_release/firmware-142pixel.bin
 
 make release PIXEL_COUNT=144
-mv out/firmware.bin out_release/firmware-144pixel.bin
+mv out/release/firmware.bin out_release/firmware-144pixel.bin
